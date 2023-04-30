8c35d0992e76130be316ea0c6fc1f9ff2006955d
Pafs001 <pafs.english@gmail.com>
2023-04-29 22:24:56 -0300

    [FIX]:bug: Corrigir porta index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js'
 import 'dotenv/config';
 
 const APPNAME = 'App Express';
-const PORT = process.env.SRV_PORT || 80;
+const PORT = process.env.SRV_PORT || 3000;
 const URL = process.env.HOST || 'localhost';
 const testMessage = `${APPNAME} Server running at http://${URL}:${PORT}/`;
 
